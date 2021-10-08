25c6f4c59dfda5cf4b5ca5a5f62c5d4d188621ab
wedhimd <wedhimd@gmail.com>
2021-10-08 16:09:44 +0200

    add navbar and home componets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import Navbar from './coomponents/Navbar';
+import Home from './coomponents/Home';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <div className="content">
+        <Home/>
+      </div>
     </div>
   );
 }
