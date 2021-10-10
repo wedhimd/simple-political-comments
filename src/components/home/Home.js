58494c80c46192fcdbdc5c051b0b006824d01732
wedhimd <wedhimd@gmail.com>
2021-10-10 11:34:24 +0200

    add error handling

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,19 +6,29 @@ import "./Home.css"
 const Home = () => {
     const [blogs, setblogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(()=>{
         fetch("http://localhost:8000/blogs")
-        .then((res)=>{
-            return res.json()
-        })
-        .then(data =>{
-            setblogs(data);
-            setIsLoading(false)
-        })
-    },[])
+            .then((res)=>{
+                if(!res.ok){
+                    throw Error("Something went wrong! could not fetch that resource")
+                }
+                return res.json()
+            })
+            .then(data =>{
+                setblogs(data)
+                setIsLoading(false)
+                setError(null)
+            })
+            .catch(err =>{
+                setIsLoading(false)
+                setError(err.message)
+            })
+    }, [])
     return ( 
         <div className="home">
+            {error && <div>{error}</div>}
             {isLoading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All blogs"/>}
         </div>
