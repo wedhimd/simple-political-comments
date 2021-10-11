269091b3a1d7b0179ef19ed3c8f89d31bdfac40c
wedhimd <wedhimd@gmail.com>
2021-10-11 11:16:34 +0200

    add form for creating new blog

--- src/components/create/Create.js
+++ src/components/create/Create.js
@@ -4,6 +4,22 @@ const Create = () => {
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input type="text" 
+                    required
+                />
+                <label>Blog body:</label>
+                <textarea 
+                    required
+                ></textarea>
+                <label>Blog author:</label>
+                <select>
+                    <option value="Ali">Ali</option>
+                    <option value="Himd">Himd</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }
