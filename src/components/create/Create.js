a92b2e41f05eb60fde7554e5a51838adb8aeb330
wedhimd <wedhimd@gmail.com>
2021-10-11 11:34:53 +0200

    bind state with the appropriate input flields

--- src/components/create/Create.js
+++ src/components/create/Create.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import "./Create.css"
 
 const Create = () => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("Ali");
+
+
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
@@ -8,17 +14,27 @@ const Create = () => {
                 <label>Blog title:</label>
                 <input type="text" 
                     required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
                 <label>Blog body:</label>
                 <textarea 
                     required
+                    value= {body}
+                    onChange = {(e)=> setBody(e.target.value)}
                 ></textarea>
                 <label>Blog author:</label>
-                <select>
+                <select
+                    value={author}
+                    onChange = {(e)=> setAuthor(e.target.value)}
+                >
                     <option value="Ali">Ali</option>
                     <option value="Himd">Himd</option>
                 </select>
                 <button>Add Blog</button>
+                <p>{title}</p>
+                <p> {body} </p>
+                <p> {author} </p>
             </form>
         </div>
      );
